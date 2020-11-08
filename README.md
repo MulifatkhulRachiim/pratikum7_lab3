6e034b4c6169b0765793d72b4de889357e9c381b
mulifarach <mulifatkhul.rach@gmail.com>
2020-11-08 20:56:17 +0700

    update

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # pratikum7_lab3
 Repository ini dibuat untuk memenuhi tugas pratikum 7 lab 3
+
+
+**Nama : Farah***
---
+++ lab3_lat2.py
@@ -0,0 +1,8 @@
+n=int(input("masukkan Nilai N : "))
+print("\n")
+import random
+
+for m in list(range(1,n+1,1)):
+    print(random.uniform(0,0.5))
+print()
+
